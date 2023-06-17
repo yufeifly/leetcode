1fd4af008eadec19537e0951e15688e91a86b628
yufeifly <yufei.xiong@qq.com>
2023-06-17 15:36:52 +0800

    解注释

--- go/137.go
+++ go/137.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"fmt"
+	"sort"
+
 )
 
-//func singleNumber(nums []int) int {
-//	sort.Ints(nums)
-//	pin := nums[0]
-//	for i := 2; i < len(nums); i += 3 {
-//		if nums[i] != pin {
-//			return pin
-//		} else {
-//			pin = nums[i+1]
-//		}
-//	}
-//	//fmt.Println(nums)
-//	return pin
-//}
+func singleNumber(nums []int) int {
+	sort.Ints(nums)
+	pin := nums[0]
+	for i := 2; i < len(nums); i += 3 {
+		if nums[i] != pin {
+			return pin
+		} else {
+			pin = nums[i+1]
+		}
+	}
+	//fmt.Println(nums)
+	return pin
+}
 
 func main() {
 	vec := []int{0, 1, 0, 1, 0, 1, 99}
